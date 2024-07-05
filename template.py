44632205c5395d088e0aa44bf2fef1ff05f686d5
Neha Vishwakarma <nehavishwakarma7777@gmail.com>
2024-07-05 15:35:40 +0530

    update

---
+++ template.py
@@ -0,0 +1,35 @@
+
+import os
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s ")
+
+package_name = "DeepClassifier"
+
+list_of_files =[
+    ".github/workflows/.gitkeep",
+    "src/__init__.py",
+    "src/run_local.py",
+    "src/helper.py",
+    "model/instruction.txt",
+    "requirements.txt",
+    "setup.py",
+    "main.py",
+    "research/trials.ipynb"
+]
+
+for filepath in list_of_files:
+    filepath =Path(filepath)
+    filedir, filename = os.path.split(filepath)
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Crete directory: {filedir} for file: {filename}")
+
+    if (not os.path.exists(filepath) or (os.path.getsize(filepath) ==0)):
+        with open(filepath,"w") as f:
+            pass  # empty file
+            logging.info(f"Creating empty file : {filepath}")
+
+    else:
+        logging.info(f"{filename} already exist")
